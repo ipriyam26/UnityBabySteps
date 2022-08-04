5ad1ff4c0f57a28547b84fd4d3d61e1de1407c97
ipriyam26 <ipriyam26@gmail.com>
2022-08-05 01:05:20 +0530

    first commit

---
+++ Assets/Scenes/Driver.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+Vector3 direction = new Vector3(5, 0, 0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move to the right every third frame
+
+
+            transform.Translate(direction*Time.deltaTime);
+            // rotate every after 10 frames
+            if (Time.frameCount % 100 == 0)
+            {
+            transform.Rotate(0, 0, Random.Range(-180, 180));
+            }
+
+
+        
+
+    }
+}
